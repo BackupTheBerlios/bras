54d201d7b4f10bbd058ec37c61af5467e281c671
kir <kir>
2000-09-16 16:22:11 +0000

    This is 0.99.8

--- bras/pkgIndex.tcl
+++ bras/pkgIndex.tcl
@@ -1,5 +1,5 @@
 namespace eval ::bras {
-  variable VERSION 0.99.7
+  variable VERSION 0.99.8
   variable VERDATE 0000-00-00
   package provide bras $VERSION
   variable base [file dir [info script]]
