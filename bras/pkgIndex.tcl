643fbc1fa5b1819aa3699fd38aff3cf70c9f9664
kir <kir>
2001-10-21 17:28:10 +0000

    This is going to be 1.4.1. I will not be published since it does not contain new features.
    The file pkgIndex.tcl is no longer autogenerated but prepared by
    hand. It contains now the version number and allows to get rid of
    the file .version.
    
    Finally removed subdirectories Test1, Test2 which were not used for
    quite some time now.
    
    Removed the non-feature of bras being independent of the package. This
    was only a half-solution because it did not take rule-files into
    account.

--- bras/pkgIndex.tcl
+++ bras/pkgIndex.tcl
@@ -1,4 +1,20 @@
-namespace eval ::bras {
-  variable VERSION 1.4.0
-  variable VERDATE 0000-00-00
-}
+## tclPkgUnknown, when running this script, makes sure that
+## $dir is set to the directory of this very file
+
+set VERSION 1.4.1
+set VERDATE 0000-00-00
+package ifneeded bras $VERSION \
+    [concat \
+	 source [file join $dir bras.tcl] \; \
+	 source [file join $dir consider.tcl] \; \
+	 source [file join $dir evalCmds.tcl] \; \
+	 source [file join $dir exported.tcl] \; \
+	 source [file join $dir lastMinuteRule.tcl] \; \
+	 source [file join $dir makeRule.tcl] \; \
+	 source [file join $dir predicates.tcl] \; \
+	 source [file join $dir sourceDeps.tcl] \; \
+	 source [file join $dir cvsknown.tcl] \; \
+ 	 package provide bras $VERSION \;\
+	 namespace eval bras [list set VERSION $VERSION] \; \
+	 namespace eval bras [list set VERDATE $VERDATE] \;
+	]
