55a5998a53c4f6ae375db4c0874df391458ca0eb
pifpafpuf <pifpafpuf>
2001-12-30 09:39:22 +0000

    This went out as 2.0 to berlios.

--- bras/pkgIndex.tcl
+++ bras/pkgIndex.tcl
@@ -1,7 +1,10 @@
+
+# $Revision: 1.37 $, $Date: 2001/12/30 09:39:22 $
+
 ## tclPkgUnknown, when running this script, makes sure that
 ## $dir is set to the directory of this very file
 
-set VERSION 1.4.1
+set VERSION 2.0
 set VERDATE 0000-00-00
 package ifneeded bras $VERSION \
     [concat \
